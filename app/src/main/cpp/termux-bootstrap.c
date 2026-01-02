553f783eaccad1d535cf7a1dd324856697ff64c8
𝙸𝚊𝚝𝚑𝚎𝚒𝚢𝚊 <iatheiya@gmail.com>
2026-01-02 16:03:20 +0100

    Update termux-bootstrap.c

--- app/src/main/cpp/termux-bootstrap.c
+++ app/src/main/cpp/termux-bootstrap.c
@@ -1,11 +1,10 @@
 #include <jni.h>
 #include <stdint.h>
 
-extern char blob[] __attribute__((visibility("hidden")));
-extern int blob_size __attribute__((visibility("hidden")));
+extern const unsigned char blob[] __attribute__((visibility("hidden")));
+extern const int blob_size __attribute__((visibility("hidden")));
 
-JNIEXPORT jbyteArray JNICALL Java_com_termux_app_TermuxInstaller_getZip(JNIEnv *env, jobject This)
-{
+JNIEXPORT jbyteArray JNICALL Java_com_termux_app_TermuxInstaller_getZip(JNIEnv *env, jobject This) {
     jsize size = (jsize)blob_size;
     jbyteArray ret = (*env)->NewByteArray(env, size);
     if (ret == NULL) return NULL;
