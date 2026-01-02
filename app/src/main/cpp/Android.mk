ed2f8b320355cb7975a3d2e189b03f0e2965603b
𝙸𝚊𝚝𝚑𝚎𝚒𝚢𝚊 <iatheiya@gmail.com>
2026-01-03 00:26:05 +0100

    Update Android.mk

--- app/src/main/cpp/Android.mk
+++ app/src/main/cpp/Android.mk
@@ -6,10 +6,21 @@ BOOTSTRAP_SRC := termux-bootstrap-zip.S
 
 LOCAL_SRC_FILES := $(BOOTSTRAP_SRC) termux-bootstrap.c
 LOCAL_MODULE := libtermux-bootstrap
-# Cleaned up flags, removed LOCAL_ARM_MODE to prevent forcing 32-bit/thumb on mixed builds
 LOCAL_CFLAGS += -Os -fno-stack-protector -I$(LOCAL_PATH) -fvisibility=hidden -Wall -Wextra -std=c11
 LOCAL_ASFLAGS += -I$(LOCAL_PATH)
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)
 LOCAL_LDFLAGS += -Wl,--no-undefined
+
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := termux_loader
+LOCAL_SRC_FILES := termux_loader.c
+LOCAL_CFLAGS += -fPIC -fPIE -DANDROID -Os -fvisibility=hidden -Wall -Wextra -std=c11 -fno-stack-protector
+LOCAL_ASFLAGS += -I$(LOCAL_PATH)
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)
+LOCAL_LDLIBS := -llog
+LOCAL_LDFLAGS += -Wl,-z,now -Wl,-z,relro -Wl,--no-undefined
 
 include $(BUILD_SHARED_LIBRARY)
